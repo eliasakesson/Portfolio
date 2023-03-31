c31f4ed63fa63670bd51e92fa1751aca9ab55582
Swezyn <akessonelias@gmail.com>
2023-03-31 16:44:46 +0200

    Updated About Me

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -6,7 +6,7 @@ const About = () => {
         <section className="about">
             <h2><span className='number'>01.</span>About Me</h2>
             <RevealAnim type='translateY' delay='0.2s'>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi veritatis debitis ut consequatur explicabo, deleniti officiis accusamus corporis maxime aut. Quasi et, similique iure iusto nostrum nihil suscipit optio eum.</p>
+                <p>Hello! My name is Elias and I enjoy programming and designing things. I'm currently at Finnvedens Gymnasium studying information and media technology.</p>
             </RevealAnim>
         </section>
     )
